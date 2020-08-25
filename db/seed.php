0c1c3ede9f8480bd4068fef9784b369b612439a8
Anjana <anjanapknr@gmail.com>
2020-08-25 09:27:04 -0700

    add registration form,change default template

--- db/seed.php
+++ db/seed.php
@@ -3,6 +3,12 @@ $database=new SQLite3('project.db');
 $pdo=new PDO("sqlite:project.db");
 $query=file_get_contents("db.sql");
 $stringArray=explode(";",$query);
-foreach($stringArray as $q){
-    $pdo->exec($q);
+foreach($stringArray as $q)
+
+{
+    if(!ctype_space($q)&& $q!="")
+    {
+        $pdo->exec($q);
+    }
+    
 }
--- templates/layout/default.php
+++ templates/layout/default.php
@@ -10,14 +10,25 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 </head>
 <body>
-    <nav class="top-nav">
-        <div class="top-nav-title">
-            <a href="<?= $this->Url->build('/') ?>"><span>indigo</span>airline</a>
-        </div>
-        <div class="top-nav-links">
-            
-        </div>
-    </nav>
+    
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand text-primary" href="<?= $this->Url->build('/') ?>">INDIGO AIRLINES</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="<?= $this->Url->build('/') ?>">Home <span class="sr-only">(current)</span></a>
+      </li>
+      
+      
+    </ul>
+    
+  </div>
+</nav>
     <main class="main">
         <div class="container">
             <?= $this->Flash->render() ?>
@@ -28,3 +39,5 @@
     </footer>
 </body>
 </html>
+
+
