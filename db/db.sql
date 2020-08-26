ff522eaac4819e9ed41d651407b6b9e18c11a841
Anjana <anjanapknr@gmail.com>
2020-08-26 04:04:12 -0700

    fixed bug in seed.php

--- db/db.sql
+++ db/db.sql
@@ -1,5 +1,5 @@
 
-create table admins(id int primary key,username varchar(20),password varchar(15),email_id varchar(20));
-create table flights(id int primary key autoincrement,price real,date varchar(30),from_date varchar(50),to_date varchar(50),counter varchar(30),departs varchar(30),arrives varchar(50));
-create table bookings(id int primary key autoincrement,user_id int,status int,discount real);
-create table users(id int primary key autoincrement,email_id varchar(20),first_name varchar(20),last_name varchar(20),address varchar(40),passport_details varchar(30),state varchar(30),country varchar(30));
+create table admins(id integer primary key,username varchar(20),password varchar(15),email_id varchar(20));
+create table flights(id integer primary key autoincrement,price real,date varchar(30),from_date varchar(50),to_date varchar(50),counter varchar(30),departs varchar(30),arrives varchar(50));
+create table bookings(id integer primary key autoincrement,user_id integer,status integer,discount real);
+create table users(id integer primary key autoincrement,email_id varchar(20),first_name varchar(20),last_name varchar(20),address varchar(40),passport_details varchar(30),state varchar(30),country varchar(30));
--- db/seed.php
+++ db/seed.php
@@ -1,14 +1,8 @@
 <?php
 $database=new SQLite3('project.db');
-$pdo=new PDO("sqlite:project.db");
 $query=file_get_contents("db.sql");
-$stringArray=explode(";",$query);
-foreach($stringArray as $q)
 
-{
-    if(!ctype_space($q)&& $q!="")
-    {
-        $pdo->exec($q);
-    }
+        $database->exec($query);
     
-}
+    
+
