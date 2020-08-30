cbda908d63150c794c19b1c4ec6f4ff73ae973f6
Anjana <anjanapknr@gmail.com>
2020-08-30 05:12:33 -0700

    bugging database

--- db/db.sql
+++ db/db.sql
@@ -1,6 +1,6 @@
 
 create table admins(id integer primary key,username varchar(20),password varchar(15),email varchar(20));
-create table flights(id integer primary key autoincrement,aircraft_type id integer,price real,date varchar(30),from_date varchar(50),to_date varchar(50),counter varchar(30),departs varchar(30),arrives varchar(50));
+create table flights(id integer primary key autoincrement,aircraft_type_id integer,price real,date varchar(30),from_date varchar(50),to_date varchar(50),counter varchar(30),departs varchar(30),arrives varchar(50));
 create table bookings(id integer primary key autoincrement,user_id integer,flight_id integer,status integer, frequent_flyer_discount real,class_id integer,seat_no integer);
 create table users(id integer primary key autoincrement,email varchar(20),first_name varchar(20),last_name varchar(20),address varchar(40),passport_details varchar(30),state varchar(30),country varchar(30));
 create table special_fare(id integer primary key,category varchar(30),discount_percentage real);
