f999c4756294e981e2b5a37f828a471a75dbb2f3
anjan152 <anjanapknr@gmail.com>
2021-01-21 11:21:16 +0530

    Bug fixes

--- src/Controller/BookingsController.php
+++ src/Controller/BookingsController.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace App\Controller;
@@ -112,37 +113,68 @@ class BookingsController extends AppController
 
         return $this->redirect(['action' => 'index']);
     }
-    public function buy($id=null,$classId, $scheduleId)
+    public function buy($id = null, $classId, $scheduleId)
     {
         $this->Authorization->skipAuthorization();
-        $lastSeatNo = $this->getLastSeatNumber($id);
-        $booking=$this->Bookings->newEmptyEntity();
-        $booking->flight_id=$id;
-        $user=$this->Authentication->getIdentity();
-        $booking->user_id=$user->id;
-        $booking->frequent_flyer_discount=10;
-        $booking->class_id=$classId;
-        $booking->flight_schedule_id =$scheduleId;
-        $booking->seat_no=$lastSeatNo + 1;
-        $booking->date = date("Y/m/d");
-        if($this->Bookings->save($booking))
-        {
-            $this->Flash->success(__('The flight has been booked.'));
+        $lastSeatNo = $this->getLastSeatNumber($id, $classId, $scheduleId);
 
+        $flight = $this->Bookings->Flights->get($id);
+        $flightClass = $this->Bookings->FlightClasses->get($classId);
+        $aircraft = $this->Bookings->Flights->AircraftTypes->get($flight->aircraft_type_id);
+
+        if ($flightClass->class == "First Class") {
+            $totalSeats = $aircraft->first_class;
+        } else if ($flightClass->class == "Second Class") {
+            $totalSeats = $aircraft->second_class;
+        } else {
+            $totalSeats = $aircraft->third_class;
         }
-        else
-        {
-        $this->Flash->error(__('The flight could not be booked. Please, try again.'));
+
+        if ($totalSeats <= $lastSeatNo) {
+            $this->Flash->error(__('The flight has been fully booked for that class at that time'));
+        } else {
+            $user = $this->Authentication->getIdentity();
+            $discount = $this->getFrequentFlierDiscount($user->id);
+
+            $booking = $this->Bookings->newEmptyEntity();
+            $booking->flight_id = $id;
+            $booking->user_id = $user->id;
+            $booking->frequent_flyer_discount = $discount;
+            $booking->class_id = $classId;
+            $booking->flight_schedule_id = $scheduleId;
+            $booking->seat_no = $lastSeatNo + 1;
+            $booking->date = date("Y/m/d");
+            if ($this->Bookings->save($booking)) {
+                $this->Flash->success(__('The flight has been booked.'));
+            } else {
+                $this->Flash->error(__('The flight could not be booked. Please, try again.'));
+            }
         }
-        return $this->redirect(['action'=>'index']);
-        
+
+
+        return $this->redirect(['action' => 'index']);
     }
 
-    public function getLastSeatNumber($id){
-        $flight = $this->Bookings->Flights->find('all')->where(['Flights.id ='=>$id]);
-        $bookingsCount = $this->Bookings->find('all')->where(['Bookings.flight_id ='=>$id])->count();
+    public function getLastSeatNumber($id, $classId, $scheduleId)
+    {
+        $bookingsCount = $this->Bookings->find('all')
+            ->where(['Bookings.flight_id =' => $id])
+            ->where(['Bookings.class_id =' => $classId])
+            ->where(['Bookings.flight_schedule_id =' => $scheduleId])
+            ->count();
 
         return $bookingsCount;
     }
 
+    public function getFrequentFlierDiscount($userId)
+    {
+        $bookingsCount = $this->Bookings->find('all')
+            ->where(['Bookings.user_id =' => $userId])
+            ->count();
+        $discount = 0;
+        if ($bookingsCount > 100) {
+            $discount = $bookingsCount / 10;
+        }
+        return $discount;
+    }
 }
--- templates/FlightSchedules/select.php
+++ templates/FlightSchedules/select.php
@@ -1,11 +1,11 @@
 <div class="flightSchedules index content">
     
-    <h3><?= __('Please select a Flight Class') ?></h3>
+    <h3><?= __('Please select a Time And Day') ?></h3>
   
             
                 <?php foreach ($flightSchedules as $index => $flightSchedule): ?>
             
-                     <?= $index ?>.   <?= $this->Form->postLink(__($flightSchedule->departs . " - " .  $flightSchedule->arrives), ['controller'=>'Bookings','action' => 'buy', $flightId,$classId, $flightSchedule->id], ['confirm' => __('Are you sure  ?', $flightSchedule->id)]) ?>
+                     <?= $index + 1 ?>.   <?= $this->Form->postLink(__($flightSchedule->day . " : " . $flightSchedule->departs . " - " .  $flightSchedule->arrives), ['controller'=>'Bookings','action' => 'buy', $flightId,$classId, $flightSchedule->id], ['confirm' => __('Are you sure  ?', $flightSchedule->id)]) ?>
                        
                 <br>
                 <?php endforeach; ?>
